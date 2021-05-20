fb02ca04965b1d980bec28a78c165393a5379e05
j7amo <roman.tarnakin@gmail.com>
2021-05-20 16:19:54 +0300

    Начнёт использовать props в модуле Post, что позволит передавать извне содержимое поста вместо захардкоденного значения в JSX-разметке.

--- social-network/src/components/Profile/MyPosts/MyPosts.jsx
+++ social-network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -10,8 +10,8 @@ const MyPosts = () => {
             <button>Add post</button>
           </div>
           <div className={classes.existingPosts}>
-            <Post />
-            <Post />
+            <Post message='Пошли бухать!'/>
+            <Post message='Привет! Как дела?'/>
           </div>
         </div>
   );
--- social-network/src/components/Profile/MyPosts/Post/Post.jsx
+++ social-network/src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import classes from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
       <div className={classes.post}>
         <img src='https://forums.ultra-combo.com/uploads/default/original/2X/6/6a0f33ec1ed9a221290497c143bdbf3e4763df91.jpg' alt='avatar'/>
-        <div className={classes.postContent}> Goodbye Earth!</div>
+        <div className={classes.postContent}>{props.message}</div>
       </div>
   );
 };
